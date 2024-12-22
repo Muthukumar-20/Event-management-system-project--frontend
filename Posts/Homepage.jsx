42f20d1543403801306ed539c70ba7d8b6020fb2
Muthu kumar <mk2778555@gmail.com>
2024-12-22 23:45:13 +0530

    update

--- Posts/Homepage.jsx
+++ Posts/Homepage.jsx
@@ -9,28 +9,10 @@ import App from '../src/App';
 
 
 const Homepage = ({ isLoggedIn }) => {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const { userData } = useContext(UserDetailsContext);
-
-
-  //  const handleLogin = () => {
-  //   setIsLoggedIn(true);
-  // };
-  // const handleLogout = () => {
-  //   setIsLoggedIn(false);
-  // };
-
-  //  useEffect(()=>{
-  // handleLogin()
-  // handleLogout()
-
-  //  },[])
-
-  // (userData);
 
   const data = [
     {
-      image: '../src/images/eventimag.jpg',
+      image: 'https://img.freepik.com/premium-photo/celebration-event-illustration_941097-88655.jpg',
       title: 'New year celebration',
     },
     {
@@ -38,7 +20,7 @@ const Homepage = ({ isLoggedIn }) => {
       title: 'Music Consert',
     },
     {
-      image: '../src/images/event-management.jpg',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStpB7qsWHlSwAn53jbnAst3ztQsuiKDL6N0g&s',
       title: 'Wedding aniversary',
     },
     {
@@ -116,15 +98,6 @@ const Homepage = ({ isLoggedIn }) => {
             </div>
           </div>
 
-          {/* <span className='absolute'>
-            <input className=' px-4 py-3 w-[500px]  backdrop-blur-[1px] text-black rounded-xl border-2 border-orange-400 font-[sans-serif] focus:w-[600px] focus:outline-none focus:border-blue-600 transition-all duration-200 ease-linear' type="email" placeholder="Search events..." />
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="16px" className="z-20 absolute right-4 font-bold size-5 top-4">
-              <path
-                d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z">
-              </path>
-            </svg>
-
-          </span> */}
 
 
           <header className='flex py-4 px-4 sm:px-1 font-[sans-serif] min-h-[70px] tracking-wide absolute top-0 w-fit right-0'>
